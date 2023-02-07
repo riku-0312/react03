4b73959f90056f3517d5687eb06c780044e890c5
riku-0312 <114901448+riku-0312@users.noreply.github.com>
2023-02-08 01:36:39 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import { fetchImages } from "./api";
+
+function Header() {
+  return (
+    <header className="hero is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Cute Bird Images</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function Image(props) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image">
+        <img src={props.src} alt="cute dog!" />
+        </figure>
+      </div>
+    </div>
+  );
+}
+
+function Loading() {
+  return <p>Loading...</p>;
+}
+
+function Gallery(props) {
+  const { urls } = props;
+  if (urls == null) {
+    return <Loading />;
+  }
+  return (
+    <div className="columns is-vcentered is-multiline">
+      {urls.map((url) => {
+        return (
+          <div key={url} className="column is-14">
+            <Image src={url} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function Main() {
+  const [urls, setUrls] = useState(null);
+  useEffect(() => {
+    fetchImages("bird").then((urls) => {
+      setUrls(urls);
+    });
+  }, []);
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <Gallery urls={urls}/>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>Bird images are retrieved from Birds API</p>
+        <p>
+          <a href="https://shibe.online/">Donate to Birds API</a>
+        </p>
+        <p>    
+          <a href="https://shibe.online/api/birds">Donate to Birds images</a>
+        </p>
+        <p>
+        　情報科学科　Webプログラミング演習課題　5421082　志村陸
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
